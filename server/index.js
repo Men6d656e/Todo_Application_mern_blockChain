d139508f596cff5dda47f6fef2a67f6601ecaa17
Men6d656e <men6d656e@gmail.com>
2025-03-23 12:17:51 +0500

    basic concept clear make 2 apis to comunicate with the smart contract with backend

--- server/index.js
+++ server/index.js
@@ -2,14 +2,69 @@
 
 const express = require("express");
 const ABI = require("./ABI.json");
-const Web3 = require("web3");
+const {Web3} = require("web3");
 
 const app = express();
 
-const web3 = new Web3()
+const web3 = new Web3("https://crimson-wider-brook.ethereum-sepolia.quiknode.pro/098488dc3702aa4eef450580158db0fc0317eb19/");
 
+const contractAddress = "0x16B8122FbD61c743067E951c80c2908383Bf3156";
 
+const contract = new web3.eth.Contract(ABI,contractAddress);
+// console.log(contract);
 
+// test the program
+// const viewTask = async (req , res ) => {
+//     const task = await contract.methods.viewTask(2).call();
+//     console.log(task);
+// }
+// viewTask();
+
+app.get("/api/etherum/view-task/:taskId" , async (req,res) => {
+   try {
+     const taskId = req.params.taskId;
+     const task = await contract.methods.viewTask(taskId).call();
+     console.log(task);
+     const {id,name,date} = task;
+     const NumId = Number(id);
+     const taskObj ={
+        NumId,
+        name,
+        date
+     }
+
+     return res.status(200).json({status:200,taskObj,message:"task Exist"});
+   } catch (error) {
+       console.log(error);
+       return res.status(500).json({error:"Internal server error"});
+    
+   }
+});
+
+app.get("/api/etherum/view-all-task", async (req,res) => {
+  try {
+    const task = await contract.methods.allTask().call();
+    console.log(task);
+    if(task.length<0){
+      return res.status(400).json({message: "no tasks yet!"})
+    }
+    if(task.length > 0){
+      const TaskList = task.map((task) => {
+        return {
+          id:Number(task.id),
+          name:task.name,
+          date:task.date
+        }
+      })
+      console.log(TaskList)
+      return res.status(200).json({status:200,TaskList,message:"All task"});
+    }
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({error:"Internal server error"}); 
+  }
+})
 
 
 
